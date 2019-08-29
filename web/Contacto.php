7cb7a3e8e7eacf923914b0845e81d6d0312be8d2
Emiliano Policardo <emilianopolicardo@gmail.com>
2019-08-28 23:21:38 -0300

    Cambio a web completa

---
+++ web/Contacto.php
@@ -0,0 +1,16 @@
+<?php
+$nombre= $_GET['nombre'];
+$edad= $_GET['edad'];
+$email= $_GET['email'];
+$carrera= $_GET['carrera'];
+echo "<h2>Información recibida </h2>";
+echo "el nombre recibido es:" . $nombre . "<br/>";
+
+echo "el edad recibido es:" . $edad . "<br/>";
+
+echo "el email recibido es:" . $email . "<br/>";
+
+echo " la carrera de interes es:" . $carrera . "<br/>";
+
+echo "gracias";
+?>
